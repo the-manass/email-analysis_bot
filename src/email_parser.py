581f34b589f7f46241745ca450b39882d8b0bfb6
Manas <manasamangeldi03@gmail.com>
2025-02-14 06:17:06 +0500

    Initial commit: added files/folders on project

---
+++ src/email_parser.py
@@ -0,0 +1,12 @@
+def parse_email(email_content):
+    """
+    Parses email content into structured data (dates, names, tasks).
+
+    Args:
+        email_content (str): The content of the email to be parsed.
+
+    Returns:
+        dict: A dictionary containing extracted dates, names, and tasks.
+    """
+
+parse_email()
